0abb98d94a07c51fa688995858bb37239f8202e7
arhith <67360209+arhith@users.noreply.github.com>
2021-10-17 18:59:45 +0530

    Update linear regression.py

--- linear regression.py
+++ linear regression.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv('car data.csv')
 
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+X = dataset.iloc[:,3].values
+y = dataset.iloc[:,5].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -32,16 +32,16 @@ y_pred = regressor.predict(X_test)
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+plt.title('SP vs Km Driven (Training set)')
+plt.xlabel('Km driven')
+plt.ylabel('Sp')
 plt.show()
 
 # Visualising the Test set results
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+plt.title('SP vs Km Driven (Test set)')
+plt.xlabel('Km driven')
+plt.ylabel('Sp')
 plt.show()
 
